94c7c4d2ececb35ed7a8f4b97bbc27953b87b506
liziwl <leezisy@gmail.com>
2017-12-12 16:35:18 +0800

    init

---
+++ UI/login.py
@@ -0,0 +1,62 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file '.\login.ui'
+#
+# Created by: PyQt5 UI code generator 5.9.2
+#
+# WARNING! All changes made in this file will be lost!
+
+from PyQt5 import QtCore, QtGui, QtWidgets
+
+
+class Ui_Login(object):
+    def setupUi(self, Login):
+        Login.setObjectName("Login")
+        Login.resize(500, 340)
+        Login.setMinimumSize(QtCore.QSize(500, 340))
+        Login.setMaximumSize(QtCore.QSize(500, 400))
+        self.lineEdit = QtWidgets.QLineEdit(Login)
+        self.lineEdit.setGeometry(QtCore.QRect(100, 120, 291, 41))
+        font = QtGui.QFont()
+        font.setFamily("Consolas")
+        font.setPointSize(16)
+        self.lineEdit.setFont(font)
+        self.lineEdit.setText("")
+        self.lineEdit.setObjectName("lineEdit")
+        self.pushButton = QtWidgets.QPushButton(Login)
+        self.pushButton.setGeometry(QtCore.QRect(150, 250, 191, 41))
+        font = QtGui.QFont()
+        font.setFamily("Consolas")
+        font.setPointSize(16)
+        self.pushButton.setFont(font)
+        self.pushButton.setObjectName("pushButton")
+        self.lineEdit_2 = QtWidgets.QLineEdit(Login)
+        self.lineEdit_2.setGeometry(QtCore.QRect(100, 190, 291, 41))
+        font = QtGui.QFont()
+        font.setFamily("Consolas")
+        font.setPointSize(16)
+        self.lineEdit_2.setFont(font)
+        self.lineEdit_2.setText("")
+        self.lineEdit_2.setEchoMode(QtWidgets.QLineEdit.Password)
+        self.lineEdit_2.setObjectName("lineEdit_2")
+        self.label = QtWidgets.QLabel(Login)
+        self.label.setGeometry(QtCore.QRect(70, 10, 381, 101))
+        font = QtGui.QFont()
+        font.setFamily("Consolas")
+        font.setPointSize(36)
+        font.setBold(True)
+        font.setWeight(75)
+        self.label.setFont(font)
+        self.label.setObjectName("label")
+
+        self.retranslateUi(Login)
+        self.pushButton.clicked.connect(Login.try_login)
+        QtCore.QMetaObject.connectSlotsByName(Login)
+
+    def retranslateUi(self, Login):
+        _translate = QtCore.QCoreApplication.translate
+        Login.setWindowTitle(_translate("Login", "Login"))
+        self.lineEdit.setPlaceholderText(_translate("Login", "Email address"))
+        self.pushButton.setText(_translate("Login", "Login"))
+        self.lineEdit_2.setPlaceholderText(_translate("Login", "Password"))
+        self.label.setText(_translate("Login", "Security IM"))
---
+++ UI/main.py
@@ -0,0 +1,20 @@
+from PyQt5 import QtWidgets, QtGui
+import sys
+
+from login import Ui_Login
+
+
+class mywindow(QtWidgets.QWidget, Ui_Login):
+    def __init__(self):
+        super(mywindow, self).__init__()
+        self.setupUi(self)
+
+    # 定义槽函数
+    def try_login(self):
+        pass
+
+
+app = QtWidgets.QApplication(sys.argv)
+window = mywindow()
+window.show()
+sys.exit(app.exec_())
