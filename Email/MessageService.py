ce79bede0ba6aaa8a886b652de913e7950ddfca0
WangGY <11510050@mail.sustc.edu.cn>
2018-01-01 16:38:40 +0800

    updateing mailsercice and MessageService

--- Email/MessageService.py
+++ Email/MessageService.py
@@ -5,57 +5,69 @@ import copy
 # for testing
 import rsa
 import os
+import Main
+import threading
+import time
 
 # !! just fot test
 # we need this from KeyService
 (pubkey, privkey) = rsa.newkeys(512)
 
 
+# folder: 'INBOX'
+# message [mail]
+
 class MessageServiceInterface:
     privkey = None
     user_config = None
     mailservice = None
+    listener = None
 
-    # send a message to a single account.
-    def send_message(self, accounts, message, attachments_path):
+    # send a message to several accounts.
+    def send_message(self, accounts, message):
         raise NotImplementedError
 
-    # check the subject to decide whether to decrypt the message
-    def get_message(self, folder):
+    # get all mails in 'INBOX' with subject as uuid
+    def get_all_messages(self, uuid):
         raise NotImplementedError
 
-    def get_unseen_message(self, folder):
+    # get all unseen message in 'INBOX' with subject as uuid
+    def get_unseen_message(self, uuid):
         raise NotImplementedError
 
-    def get_unanswered_message(self, folder):
-        raise NotImplementedError
-
-    def get_unseen_conversion(self, accounts):
-        raise NotImplementedError
-
-    def get_all_conversion(self, accounts):
+    def notify(self, listener):
         raise NotImplementedError
 
 
 class MessageService(MessageServiceInterface):
 
-    def __init__(self, user_config):
+    def get_all_messages(self, uuid):
+
+        pass
+
+    def notify(self, listener):
+        pass
+
+    def __init__(self, user_config, listener):
         self.user_config = user_config
         self.load_privkey(None)
         self.mailservice = MailService(user_config)
 
     # need to change just return the private key object of this user
     def load_privkey(self, key_path):
+
         '''
         with open(key_path, 'rb') as f:
             key = f.read()
             f.close()
         self.privkey = KeyService.load_privkey(key)
-    '''
+        '''
+
         # (pubkey, privkey) = rsa.newkeys(512)
         self.privkey = privkey
 
     # hash function
+
     def _getuuid(self, accounts):
         accounts_name = copy.deepcopy(list(accounts))
         accounts_name.append(self.user_config['account'])
@@ -74,14 +86,22 @@ class MessageService(MessageServiceInterface):
         self.mailservice.send_mail(receiver, receivers, subject=uuid, content=ct_message,
                                    attachments=encrypted_binary_files)
 
-    def send_message(self, receivers, message, attachments_path):
-        binary_attachments = []
-        for f in attachments_path or []:
-            with open(f, 'rb') as fil:
-                binary_attachments.append({'filename': os.path.basename(f), 'data': fil.read()})
-        uid = self._getuuid(receivers)
-        for receiver in receivers:
-            self._send_message_single(receiver, receivers, message, binary_attachments, uid)
+    # def send_message(self, receivers, message, attachments_path, uid=None):
+    #     binary_attachments = []
+    #     for f in attachments_path or []:
+    #         with open(f, 'rb') as fil:
+    #             binary_attachments.append({'filename': os.path.basename(f), 'data': fil.read()})
+    #     if uid is None:
+    #         uid = self._getuuid(receivers)
+    #     for receiver in receivers:
+    #         self._send_message_single(receiver, receivers, message, binary_attachments, uid)
+
+    def send_message(self, accounts, message):
+        # just send content
+        content = message.content
+        uid = self._getuuid(accounts)
+        for recevier in accounts:
+            self._send_message_single(recevier, accounts, content, [], uid)
 
     def _decrypt_mail(self, mail):
         try:
@@ -132,11 +152,24 @@ class MessageService(MessageServiceInterface):
             msg.extend(msg_out)
         return self._get_mail_by_account(accounts, msg)
 
+    # accounts['mail address','mail address']
+    def get_mails_by_accounts(self, accounts):
+        uuid = self._getuuid(accounts)
+        return self.get_mails_by_uuid(uuid)
+
+    def get_mails_by_uuid(self, uuid):
+        msg = self.mailservice.get_mails_in_folder_with_subject('INBOX', str(uuid))
+        if msg is None:
+            return []
+        else:
+            return sorted(msg, key=lambda m: m['date'])
+
     # look for unseen message send by this accounts
     def get_unseen(self, accounts):
         msg_in = self.get_unseen_message('INBOX')
         return self._get_mail_by_account(accounts, msg_in)
 
+    # may be useless
     def get_sent(self, accounts):
         msg_out = self.get_message('已发送')
         return self._get_mail_by_account(accounts, msg_out)
@@ -148,7 +181,7 @@ class MessageService(MessageServiceInterface):
                 if m['subject'] == self._getuuid(accounts):
                     results.append(m)
             sorted(results, key=lambda m: m['date'])
-            return results
+        return results
 
 
 if __name__ == '__main__':
